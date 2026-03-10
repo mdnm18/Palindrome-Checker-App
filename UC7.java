72dc923ac7694fdfe5f8e623003e4c030bcdb0c1
MD NAYAJ MONDAL <md.n.m.pius18@gmail.com>
2026-03-10 10:05:58 +0530

    Implement palindrome check using Deque

---
+++ UC7.java
@@ -0,0 +1,40 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class PalindromeDeque {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "level";
+
+        // Create a Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst(); // remove from front
+            char rear = deque.removeLast();   // remove from rear
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
